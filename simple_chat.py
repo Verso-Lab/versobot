716ba2c6485f43eca9ca875fea98bbeee3c98b26
kavwad <123405488+kavwad@users.noreply.github.com>
2024-06-26 13:30:27 -0700

    initial commit

---
+++ simple_chat.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from components.chat import versobot
+from utils.session import initialize_session_states, chat_button, reset_chat
+from utils.config import set_up_page
+
+
+# Set up Streamlit app
+set_up_page("Chat")
+initialize_session_states()
+
+versobot(assistant_id="asst_i5ylsjPx1CWwe0xJFOOQ80yC",
+         initial_message="Hi, I'm **Versobot!** How can I help?")
