9c599f55577cf0a6347fdc7ca1bb620b7412f07e
kavwad <123405488+kavwad@users.noreply.github.com>
2024-08-24 13:27:01 -0700

    fix: rename pages/ -> app_pages/ to avoid conflict

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.config import set_up_page
 
 pg = st.navigation([
-    st.Page("pages/prompting_tips.py", title="Prompting tips", icon=":material/forum:"),
-    st.Page("pages/summarization.py", title="Summarization [buggy]", icon=":material/summarize:")
+    st.Page("app_pages/prompting_tips.py", title="Prompting tips", icon=":material/forum:"),
+    st.Page("app_pages/summarization.py", title="Summarization [buggy]", icon=":material/summarize:")
 ])
 pg.run()
